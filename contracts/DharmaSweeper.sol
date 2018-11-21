69cae15845917cb7dbbe0e82e7d980c14b5db95e
Matt Tyndall <mattctyndall@gmail.com>
2018-11-20 20:18:05 -0500

    fix conflict

--- contracts/DharmaSweeper.sol
+++ contracts/DharmaSweeper.sol
@@ -5,7 +5,6 @@ pragma solidity ^0.4.24;
  * https://github.com/austintgriffith/counterfactual-token-repayment/blob/master/contracts/Sweeper/Sweeper.sol
  *
  * Upon creation, this contract will make a repayment on behalf of a debtor
-<<<<<<< HEAD
  *
  * agreementId - Dharma loan agreement ID
  * repaymentRouter - Address of Dharma's RepaymentRouter contract
@@ -13,8 +12,6 @@ pragma solidity ^0.4.24;
  * tokenAddress - Address of ERC20 token being repaid
  * gasRefund - Address that any remaining ETH should be returned to
  * amount - Amount of ERC20 token being repaid
-=======
->>>>>>> 20a51f010ccaa8a48c0fd3b98a75fcfcb5bffacf
  */
 contract DharmaSweeper {
 
@@ -23,10 +20,7 @@ contract DharmaSweeper {
         address repaymentRouter,
         address tokenTransferProxy,
         address tokenAddress,
-<<<<<<< HEAD
         address gasRefund,
-=======
->>>>>>> 20a51f010ccaa8a48c0fd3b98a75fcfcb5bffacf
         uint256 amount
     )
         public
@@ -36,11 +30,7 @@ contract DharmaSweeper {
         // make the repayment
         IRepaymentRouter(repaymentRouter).repay(agreementId, amount, tokenAddress);
         // destroy contract
-<<<<<<< HEAD
         selfdestruct(gasRefund);
-=======
-        selfdestruct(tokenTransferProxy);
->>>>>>> 20a51f010ccaa8a48c0fd3b98a75fcfcb5bffacf
     }
 }
 
